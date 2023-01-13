80deac91a3a06e1b33e911012ffa4152b1e9dde5
Gabriel Mangiurea <gabriel.mangiurea+git@outlook.com>
2023-01-14 01:31:55 +0200

    chore(authentication): add PRINCIPAL to the jwt guard

--- src/authentication/guards/jwt-authentication.guard.ts
+++ src/authentication/guards/jwt-authentication.guard.ts
@@ -1,12 +1,17 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { ClsService } from 'nestjs-cls';
 
+import { PRINCIPAL_TOKEN } from 'src/audit/audit.constants';
 import { SKIP_JWT_AUTHENTICATION_TOKEN } from '../authentication.decorators';
 
 @Injectable()
 export class JwtAuthenticationGuard extends AuthGuard('jwt') {
-  constructor(private readonly reflector: Reflector) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly clsService: ClsService,
+  ) {
     super();
   }
 
@@ -18,5 +23,16 @@ export class JwtAuthenticationGuard extends AuthGuard('jwt') {
 
     if (isJWTSkipped) return true;
     return super.canActivate(context);
+  }
+
+  handleRequest<User = any>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): User {
+    this.clsService.set(PRINCIPAL_TOKEN, user.sub);
+    return user;
   }
 }
